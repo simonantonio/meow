055598a85a13f57233f34e593a6ee57ff16b0403
santonio <simon@pyriahsoft.com>
2021-09-22 16:12:19 +0800

    meow mk1

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hmmm
+{
+    public partial class Form1 : Form
+    {
+        private readonly GlobalHotkey _ghk;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+
+            //must be after show in taskbar is set false - as this changes handle
+            _ghk = new GlobalHotkey(Program.Constants.NOMOD, Keys.Delete, this);
+
+            this.Load += (o, e) =>
+            {
+                var reg = _ghk.Register();
+
+                if (!reg)
+                    System.Diagnostics.Debug.WriteLine("Failed to register");
+
+                this.Size = Size.Empty;
+            };
+
+            this.Closing += (o, e) =>
+            {
+                _ghk.Unregiser();
+            };
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Program.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private int _keyCounter = 0;
+        private void HandleHotkey()
+        {
+            _keyCounter++;
+            if (_keyCounter > 10)
+            {
+                //play sound;
+                System.Diagnostics.Debug.WriteLine("Hotkey pressed");
+                var audio = new SoundPlayer(Resource2.ResourceManager.GetStream("Meow"));
+                audio.Play();
+            }
+        }
+
+    }
+}
---
+++ GlobalHotkey.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace Hmmm
+{
+
+    internal class GlobalHotkey
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private readonly int _modifier;
+        private readonly int _key;
+        private readonly IntPtr _hWnd;
+        private readonly int _id;
+
+        public GlobalHotkey(int modifier, Keys key, Form form)
+        {
+            this._modifier = modifier;
+            this._key = (int)key;
+            this._hWnd = form.Handle;
+            _id = this.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return _modifier ^ _key ^ _hWnd.ToInt32();
+        }
+
+        public bool Register()
+        {
+            return RegisterHotKey(_hWnd, _id, _modifier, _key);
+        }
+
+        public bool Unregiser()
+        {
+            return UnregisterHotKey(_hWnd, _id);
+        }
+    }
+}
